ac308fc1c72153eacc24dee2f01104d2bc46800e
Tomasz Krysiuk <tomasz.krysiuk@gmail.com>
2017-11-25 13:05:04 +0100

    added symulator file

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #! /usr/bin/python3
+import symulator
+width, height = 800, 600
 
 def main():
+    symulator.start(width, height)
     print('in main')
 
 if __name__ == "__main__":
---
+++ symulator.py
@@ -0,0 +1,3 @@
+
+def start(width, height):
+    print('in symulator')
